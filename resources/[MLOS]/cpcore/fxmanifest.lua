39da9a1db295200553c65cb17d68b94548124267
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2023-07-02 01:26:48 +0100

    Initial Commit

---
+++ resources/[MLOS]/cpcore/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+author 'Molo Modding'
+description 'Core by Molo Modding and NeVoGiA'
+lua54 'yes'
+
+this_is_a_map 'yes'
+dependency '/assetpacks'
+
+ 
+
+files {
+    "interiorproxies.meta"
+}
+
+ 
+dependency '/assetpacks'
