ff47b0a8b5380043d419d264481178e6f9c40774
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2023-07-09 23:03:44 -0500

    Pepe full dialogue finished

---
+++ resources/[standalone]/[tsrp]/tsrp-lifepaths/Client/functions.lua
@@ -0,0 +1,11 @@
+function loadModel(model)
+    if type(model) == 'number' then
+        model = model
+    else
+        model = GetHashKey(model)
+    end
+    while not HasModelLoaded(model) do
+        RequestModel(model)
+        Citizen.Wait(0)
+    end
+end
--- resources/[standalone]/[tsrp]/tsrp-lifepaths/config.lua
+++ resources/[standalone]/[tsrp]/tsrp-lifepaths/config.lua
@@ -2,9 +2,20 @@ Config = {}
 
 Config.Debug = true
 
-Config.OrginSelectorNPC = {
+Config.LifepathSelectorNPC = {
     {
         model = 's_m_m_highsec_02',
         coords = vector4(228.28, -1006.12, -100.0, 358.85),
     },
+}
+
+Config.LifepathSettings = {
+    ["corpo"] = {
+
+    },
+    ["streetkid"] = {
+        ["SpawnLocation"] = vector4(-563.39, 285.26, 84.38, 84.83),
+        ["PepeModel"] = "s_m_m_highsec_02",
+        ["PepeLocation"] = vector4(-564.77, 284.8, 84.38, 267.56)
+    }
 }
