22b653963957cfec77c91420aece3e32917f76cb
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2023-07-28 23:26:40 -0500

    Can now use lifepaths fully

--- resources/[standalone]/[tsrp]/tsrp-lifepaths/config.lua
+++ resources/[standalone]/[tsrp]/tsrp-lifepaths/config.lua
@@ -8,3 +8,9 @@ Config.LifepathSelectorNPC = {
         coords = vector4(4.4, -1758.24, 541.37, 46.42),
     },
 }
+
+Config.StreetKidSpawnCoords = vector4(246.27, -878.97, 31.43, 64.2)
+Config.NomadSpawnCoords = vector4(1990.18, 3048.36, 47.21, 8.63)
+Config.CorpoSpawnCoords = vector4(227.15, 212.73, 105.54, 131.21)
+Config.NCPDSpawnCoords = vector4(443.85, -981.82, 31.78, 97.28)
+Config.TraumaTeamSpawnCoords = vector4(305.23, -586.95, 43.19, 65.72)
