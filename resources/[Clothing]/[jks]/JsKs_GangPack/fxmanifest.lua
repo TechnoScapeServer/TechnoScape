7a9175e660d8a8fc829b3bb85ee0d6900d6e31f3
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2023-07-31 03:45:22 +0100

    jksclothing

---
+++ resources/[Clothing]/[jks]/JsKs_GangPack/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_jsgang.meta',
+  'mp_f_freemode_01_mp_f_jsgang.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_jsgang.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_jsgang.meta'
+dependency '/assetpacks'
