de0ad126363aea3e5a78b27188d181bef45a098e
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2023-08-09 02:36:13 +0100

    Removed Duplicate Clothing Whitelist

--- resources/[standalone]/ap-clothingWhitelist/server.lua
+++
@@ -1,8 +0,0 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterNetEvent('ap-clothing:GetPlayerIdentifier', function()
-    local player = source
-    local Player = QBCore.Functions.GetPlayer(player)
-    local CitizenId = Player.PlayerData.citizenid
-    TriggerClientEvent('ap-clothing:ReturnPlayerIdentifier', player, CitizenId)
-end)
