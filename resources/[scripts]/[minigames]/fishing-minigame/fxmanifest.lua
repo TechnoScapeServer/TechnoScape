cdf7150c3f4ec034dbf2409e28781c54f5f13de3
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2023-08-10 13:37:57 -0500

    setup fishing

---
+++ resources/[scripts]/[minigames]/fishing-minigame/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+games { 'gta5' }
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+files {
+    'html/index.html',
+    'html/*.svg',
+
+    'html/index.js',
+    'html/main.css',
+}
